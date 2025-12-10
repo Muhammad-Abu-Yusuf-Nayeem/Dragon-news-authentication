c9e11bb526876b4bc3cd873aafd3c30b3c63a782
assignment-01 <ykhan9463@gmail.com>
2025-12-10 19:22:12 +0600

    Project started for authentication

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+import { Outlet } from "react-router-dom";
+
+const AuthLayout = () => {
+    return (
+        <div><h2>AuthLayout</h2>
+            <Outlet></Outlet>
+            
+        </div>
+    );
+};
+
+export default AuthLayout;
